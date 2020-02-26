d5a2779c829401d68cad08b1cba57e6350fb6b49
nidaanlama <nidaanlama999@gmail.com>
2020-02-26 16:27:56 +0300

    React Native Heart Animation

---
+++ App.js
@@ -0,0 +1,159 @@
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Animated, Dimensions, Easing } from 'react-native';
+import AntDesign  from "react-native-vector-icons/AntDesign";
+
+const { height } = Dimensions.get("window");
+
+const animationEndY = Math.ceil(height * 0.7);
+const negativeEndY = animationEndY * -1;
+
+let heartCount = 1;
+
+
+function getRandomNumber(min,max) {
+  return Math.random() * (max -min) + min
+}
+
+
+export default class App extends React.Component {
+  state = {
+    hearts: []
+  };
+
+  addHeart = () => {
+    this.setState(
+      {
+        hearts: [
+          ...this.state.hearts,
+          {
+            id: heartCount,
+            right: getRandomNumber(20,150)
+          }
+        ]
+      },
+      () => {
+        heartCount++;
+      }
+    )
+  }
+
+  removeHeart = id => {
+    this.setState({
+      hearts: this.state.hearts.filter(heart => {
+        return heart.id !== id
+      })
+    })
+  }
+
+  render() {
+    
+    return (
+      <View style={styles.container}>
+        <View style={styles.container}>
+          {this.state.hearts.map(heart => {
+            return (
+              <HeartContainer key={heart.id} style={{ right: heart.right }} onComplete={() => this.removeHeart(heart.id)} />
+            )
+          })}
+        </View>
+          <TouchableOpacity onPress={this.addHeart} style={styles.addButton}>
+            <AntDesign name="plus" size={24} color="#FFF" />
+          </TouchableOpacity>      
+      </View>
+      );
+    }
+  }
+
+  class HeartContainer extends React.Component {
+    constructor() {
+      super();
+
+      this.yAnimation = this.state.position.interpolate({
+        inputRange: [negativeEndY, 0],
+        outputRange: [animationEndY, 0]
+      });
+
+      this.opacityAnimation = this.yAnimation.interpolate({
+        inputRange: [0, animationEndY],
+        outputRange: [1, 0]
+      });
+
+      this.scaleAnimation = this.yAnimation.interpolate({
+        inputRange: [0, 15, 30],
+        outputRange: [0, 1.4, 1],
+        extrapolate: "clamp"
+      });
+
+    }
+    state = {
+      position: new Animated.Value(0)
+    }
+
+    static defaultProps = {
+      onComplete() {
+
+      }
+    }
+
+    componentDidMount() {
+
+      Animated.timing(this.state.position, {
+        duration: 2000,
+        toValue: negativeEndY,
+        easing: Easing.ease,
+        useNativeDriver: true
+      }).start(this.props.onComplete);
+      
+    }
+
+    getHeartStyle() {
+      return {
+        transform: [{ translateY: this.state.position }, { scale: this.scaleAnimation }],
+        opacity: this.opacityAnimation
+      };
+    }
+
+    render() {
+      return (
+        <Animated.View style={[styles.heartContainer, this.getHeartStyle(), this.props.style]}>
+          <Heart color="purple" />
+        </Animated.View>
+      );
+    }
+  }
+
+
+  const Heart = props => (
+    <View {...props} style={[styles.heart, props.style]}>
+        <AntDesign name="heart" size={48} color={props.color} />
+    </View>
+  )
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    addButton: {
+      backgroundColor: "#378AD9",
+      width: 60,
+      height: 60,
+      borderRadius: 30,
+      alignItems: "center",
+      justifyContent: "center",
+      position: "absolute",
+      bottom: 32,
+      left: 32
+    },
+    heartContainer: {
+      position: "absolute",
+      bottom: 30,
+      backgroundColor: "transparent"
+    },
+    heart: {
+      width: 50,
+      height: 50,
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: "transparent"
+    }
+  })
+  // Nida
